0f581561357148266438f7c72aa7e715c2b0b783
Harshal <harshallpandit@gmail.com>
2017-08-24 15:32:08 -0400

    updated bean and breakout.java

--- src/Bean.java
+++ src/Bean.java
@@ -1,4 +1,80 @@
+import java.awt.Image;
+import java.io.Serializable;
 
-public class Bean {
-
+public class Bean implements Serializable {
+	
+	private int bx;	//coordinates of ball (x, y)
+	private int by;
+	private int px;	//coordinates of paddle (x,y)
+	private int py;
+	private Image ball;
+	private Image paddle;
+	
+	public Bean()
+	{
+		bx = 0;
+		by = 0;
+		px = 0;
+		py = 0;
+	}
+	
+	int getBx()
+	{
+		return bx;
+	}
+	
+	int getBy()
+	{
+		return by;
+	}
+	
+	int getPx()
+	{
+		return px;
+	}
+	
+	int getPy()
+	{
+		return py;
+	}
+	
+	Image getBall()
+	{
+		return ball;
+	}
+	
+	Image getPaddle()
+	{
+		return paddle;
+	}
+	
+	void setBx(int x)
+	{
+		bx = x;
+	}
+	
+	void setBy(int y)
+	{
+		by = y;
+	}
+	
+	void setPx(int x)
+	{
+		px = x;
+	}
+	
+	void setPy(int y)
+	{
+		py = y;
+	}
+	
+	void setBall(Image b)
+	{
+		ball = b;
+	}
+	
+	void setPaddle(Image p)
+	{
+		paddle = p;
+	}
 }
--- src/Breakout.java
+++ src/Breakout.java
@@ -1,7 +1,16 @@
+import javax.swing.JFrame;
 
-public class Breakout {
+public class Breakout extends JFrame 
+{
 	public static void main(String[] ar)
-	{
+	{		
+		JFrame frame = new JFrame("Breakout!");
+		frame.setSize(1920, 1080);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 		
+		BreakoutUI breakoutUI = new BreakoutUI();
+		Breakout breakout = new Breakout();
+		breakout.add(breakoutUI);
 	}
 }
