b4f309fb25d6b6fbf80d75328052ba9f47c93153
Harshal <harshallpandit@gmail.com>
2017-08-24 16:31:32 -0400

    just the frame

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,16 +1,35 @@
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Image;
 
-public class Breakout extends JFrame 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Breakout  
 {
 	public static void main(String[] ar)
-	{		
-		JFrame frame = new JFrame("Breakout!");
+	{	
+		Bean bean = new Bean();
+		bean.setBx(1000);
+		bean.setBy(100);
+		bean.setPx(900);
+		bean.setPy(930);
+		Image ball = new ImageIcon("C:\\Users\\kshitij\\Desktop\\soccerball.png").getImage();
+		Image paddle = new ImageIcon("C:\\Users\\kshitij\\Desktop\\paddle.jpg").getImage();
+		bean.setPaddle(paddle);
+		bean.setBall(ball);
+		
+		BreakoutUI frame = new BreakoutUI();
 		frame.setSize(1920, 1080);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Breakout!");
+		
+		JPanel panel = new JPanel();
+		panel.setSize(1920, 1080);
+		panel.setBackground(Color.magenta);
+		frame.add(panel);
 		frame.setVisible(true);
 		
-		BreakoutUI breakoutUI = new BreakoutUI();
-		Breakout breakout = new Breakout();
-		breakout.add(breakoutUI);
+		
 	}
 }
--- src/BreakoutUI.java
+++ src/BreakoutUI.java
@@ -1,4 +1,28 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class BreakoutUI {
+import javax.swing.JFrame;
 
+public class BreakoutUI extends JFrame {
+	
+	private Bean bean = new Bean();
+	private static int ballX = 0, ballY = 0, key = 0;
+	
+	BreakoutUI(){
+		
+	}
+	
+	BreakoutUI(Bean b)
+	{
+		bean = b;
+	}
+	
+	public void paintComponents(Graphics g)
+	{
+		super.paintComponents(g);
+		ballX = bean.getBx();
+		ballY = bean.getBy();
+		g.drawImage(bean.getBall(), ballX, ballY, this);
+		System.out.println(ballX);
+	}
 }
