01eb3a7059c19cfd1c27e960c61c1a5612c8e2dd
Connor Russell <connruss@indiana.edu>
2017-08-24 20:40:24 -0400

    connor push

--- src/Brick.java
+++ src/Brick.java
@@ -1,4 +1,32 @@
+import java.awt.Graphics;
 
-public class Brick {
+public class Brick extends Bean{
 
+	private boolean destroyed;
+	
+	public Brick(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		setDestroyed(false);
+	}
+	
+	public void draw(Graphics g) {
+		if (!destroyed) {
+			g.setColor(color);
+			g.fillRect(x, y, width, height);
+		}
+	}
+	
+	public void addHit() {
+		setDestroyed(true);
+	}
+	
+
+	public boolean isDestroyed() {
+		return destroyed;
+	}
+
+	public void setDestroyed(boolean destroyed) {
+		this.destroyed = destroyed;
+	}
+	
 }
